df9bb365864f11227489872adcb616fc8e5e19c1
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-02-06 00:26:10 -0500

    Lasagna: complete

--- lasagna/src/main/java/Lasagna.java
+++ lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,17 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven() {
+        return 40;
+    }
 
-    // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int minutesSoFar) {
+        return expectedMinutesInOven() - minutesSoFar;
+    }
 
-    // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes(int layers) {
+        return 2 * layers;
+    }
 
-    // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int layers, int minutesSoFar) {
+        return preparationTimeInMinutes(layers) + minutesSoFar;
+    }
 }
