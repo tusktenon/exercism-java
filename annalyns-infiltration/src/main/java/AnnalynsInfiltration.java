4d42e16faa79806ca6af7685e2b38ac9af3c14c0
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-02-07 10:14:16 -0500

    Annalyn's Infiltration: reformat with Google Java Style

--- annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,18 +1,22 @@
 class AnnalynsInfiltration {
-    public static boolean canFastAttack(boolean knightIsAwake) {
-        return !knightIsAwake;
-    }
+  public static boolean canFastAttack(boolean knightIsAwake) {
+    return !knightIsAwake;
+  }
 
-    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        return knightIsAwake || archerIsAwake || prisonerIsAwake;
-    }
+  public static boolean canSpy(
+      boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+    return knightIsAwake || archerIsAwake || prisonerIsAwake;
+  }
 
-    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        return !archerIsAwake && prisonerIsAwake;
-    }
+  public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+    return !archerIsAwake && prisonerIsAwake;
+  }
 
-    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
-            boolean petDogIsPresent) {
-        return !archerIsAwake && (petDogIsPresent || !knightIsAwake && prisonerIsAwake);
-    }
+  public static boolean canFreePrisoner(
+      boolean knightIsAwake,
+      boolean archerIsAwake,
+      boolean prisonerIsAwake,
+      boolean petDogIsPresent) {
+    return !archerIsAwake && (petDogIsPresent || !knightIsAwake && prisonerIsAwake);
+  }
 }
