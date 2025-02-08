477d9ee4bdd253218f1060cda77e16f3b63fb737
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-02-08 17:24:40 -0500

    Salary Calculator: declare class vars for constants

--- salary-calculator/src/main/java/SalaryCalculator.java
+++ salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,10 +1,20 @@
 public class SalaryCalculator {
+  private static final int daysSkippedAllowance = 5;
+  private static final double daysSkippedPenalty = 0.15;
+  private static final int baseBonusMultiplier = 10;
+  private static final int improvedBonusMultiplier = 13;
+  private static final int improvedBonusMinimumProducts = 20;
+  private static final double baseSalary = 1000;
+  private static final double salaryCap = 2000;
+
   public double salaryMultiplier(int daysSkipped) {
-    return daysSkipped >= 5 ? 0.85 : 1.0;
+    return daysSkipped >= daysSkippedAllowance ? 1 - daysSkippedPenalty : 1.0;
   }
 
   public int bonusMultiplier(int productsSold) {
-    return productsSold >= 20 ? 13 : 10;
+    return productsSold >= improvedBonusMinimumProducts
+        ? improvedBonusMultiplier
+        : baseBonusMultiplier;
   }
 
   public double bonusForProductsSold(int productsSold) {
@@ -12,8 +22,6 @@ public class SalaryCalculator {
   }
 
   public double finalSalary(int daysSkipped, int productsSold) {
-    final double baseSalary = 1000;
-    final double salaryCap = 2000;
     final var calculatedSalary =
         baseSalary * salaryMultiplier(daysSkipped) + bonusForProductsSold(productsSold);
     return Math.min(calculatedSalary, salaryCap);
