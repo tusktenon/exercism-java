baa9ef10109cb79e6ff7333f21a745bc7a19188c
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-02-16 21:11:06 -0500

    Calculator Conundrum: complete

--- README.md
+++ README.md
@@ -30,6 +30,10 @@ Arrays, `for` loops and enhanced `for` loops. Although the intention of this exe
 
 Dates and times with the `LocalDate`, `LocalDateTime` and `DateTimeFormatter` classes.
 
+### Calculator Conundrum
+
+Exceptions.
+
 ### Captain's Log
 
 Randomness with the `Random` class. Note that in modern Java (17+), the use of `RandomGenerator` is preferred to `Random`.
--- calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,17 @@
 class CalculatorConundrum {
-    public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+  public String calculate(int operand1, int operand2, String operation) {
+    try {
+      return switch (operation) {
+        case "+" -> "%d + %d = %d".formatted(operand1, operand2, operand1 + operand2);
+        case "*" -> "%d * %d = %d".formatted(operand1, operand2, operand1 * operand2);
+        case "/" -> "%d / %d = %d".formatted(operand1, operand2, operand1 / operand2);
+        case "" -> throw new IllegalArgumentException("Operation cannot be empty");
+        case null -> throw new IllegalArgumentException("Operation cannot be null");
+        default ->
+            throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+      };
+    } catch (ArithmeticException e) {
+      throw new IllegalOperationException("Division by zero is not allowed", e);
     }
+  }
 }
