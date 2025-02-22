c3390d9237ab495d0c661a718867e6bb411ecb98
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-02-22 09:20:42 -0500

    Secret Handshake: first iteration

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class HandshakeCalculator {
 
-    List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+  List<Signal> calculateHandshake(int number) {
+    List<Signal> handshake = new ArrayList<>();
+    Signal[] signals = Signal.values();
+    for (int i = 0; i < signals.length; i++) {
+      if ((number & 1 << i) != 0) {
+        handshake.add(signals[i]);
+      }
     }
-
+    return (number & 1 << signals.length) != 0 ? handshake.reversed() : handshake;
+  }
 }
