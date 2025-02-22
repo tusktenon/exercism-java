f52c964485169efd63d444288aae15e0d3c23ec0
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-02-22 10:05:31 -0500

    Secret Handshake: add alternate loop-based solution
    Based on one of the "Dig Deeper" approaches. I find this much less
    readable than the stream-based solution. It might be faster (since it
    never has to reverse the list), but the list is so small that it can't
    matter much.

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -13,5 +14,24 @@ class HandshakeCalculator {
             .collect(Collectors.toList());
 
     return (number & 1 << signals.length) != 0 ? handshake.reversed() : handshake;
+  }
+
+  // Since this implementation does not reverse the list, it may be slightly faster (though I find
+  // it much less readable).
+  List<Signal> calculateHandshakeLoop(int number) {
+    List<Signal> handshake = new ArrayList<>();
+    Signal[] signals = Signal.values();
+    boolean reverse = (number & 1 << signals.length) != 0;
+    int start = reverse ? signals.length - 1 : 0;
+    int end = reverse ? -1 : signals.length;
+    int update = reverse ? -1 : 1;
+
+    for (int i = start; i != end; i += update) {
+      if ((number & 1 << i) != 0) {
+        handshake.add(signals[i]);
+      }
+    }
+
+    return handshake;
   }
 }
