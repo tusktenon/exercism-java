4a760e63f423da82b5e4ce36e5219639595e4867
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-05-03 17:29:39 -0400

    Luhn: first iteration

--- luhn/src/main/java/LuhnValidator.java
+++ luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,26 @@
 class LuhnValidator {
 
     boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int digitCount = 0;
+        int sum = 0;
+        for (int i = candidate.length() - 1; i >= 0; i--) {
+            int c = candidate.codePointAt(i);
+            if (Character.isDigit(c))
+                sum += luhnAdjustment(c - '0', ++digitCount);
+            else if (c == ' ')
+                continue;
+            else
+                return false;
+        }
+        return digitCount > 1 && sum % 10 == 0;
     }
 
+    private int luhnAdjustment(int digit, int count) {
+        if (count % 2 == 0) {
+            digit *= 2;
+            if (digit > 9)
+                digit -= 9;
+        }
+        return digit;
+    }
 }
